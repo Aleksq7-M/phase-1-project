20146e203d967847c224426187ce3698a6d73a35
aleksq7 <morrisalex2001@gmail.com>
2021-09-21 12:41:00 -0600

    Clicking movie list items will display said movie

--- src/index.js
+++ src/index.js
@@ -18,82 +18,107 @@ document.addEventListener('DOMContentLoaded', () => {
     document.querySelector('#list-form form').addEventListener('submit', e => {
         e.preventDefault()
     })
-})
-
-function createMovieCard(obj){
-/*Adds movie info to the screen
-Can't make it much shorter */
-let card = document.createElement('div');
-
-let title = document.createElement('h2');
-title.innerText = obj.Title;
-card.appendChild(title);
-
-let poster = document.createElement('img');
-poster.src = obj.Poster;
-card.appendChild(poster);
-
-let director = document.createElement('h2')
-director.innerText = `Directed by ${obj.Director}`;
-card.appendChild(director)
-
-let actors = document.createElement('h3');
-actors.innerText = `Starring: ${obj.Actors}`;
-card.appendChild(actors);
-
-let plot = document.createElement('p');
-plot.innerText = obj.Plot;
-card.appendChild(plot);
-
-let released = document.createElement('span');
-released.innerText = `Released: ${obj.Released}`;
-card.appendChild(released);
-
-let genre = document.createElement('span');
-genre.innerText = `Genre: ${obj.Genre}`;
-card.appendChild(genre);
 
 
-let imdb = document.createElement('p')
-imdb.innerText = `IMDB Rating: ${obj.Ratings[0].Value}`
-card.appendChild(imdb);
+    function createMovieCard(obj){
+    /*Adds movie info to the screen
+    Can't make it much shorter */
+    let card = document.createElement('div');
 
-let metascore = document.createElement('p');
-metascore.innerText = `Metascore: ${obj.Ratings[2].Value}`;
-card.appendChild(metascore);
+    let title = document.createElement('h2');
+    title.innerText = obj.Title;
+    card.appendChild(title);
 
-let rottenTomatoes = document.createElement('p')
-rottenTomatoes.innerText = `Rotten Tomatoes: ${obj.Ratings[1].Value}`;
-card.appendChild(rottenTomatoes);
+    let poster = document.createElement('img');
+    poster.src = obj.Poster;
+    card.appendChild(poster);
 
-return card;
-}
+    let director = document.createElement('h2')
+    director.innerText = `Directed by ${obj.Director}`;
+    card.appendChild(director)
 
-function makeList(obj){
-    let listList = document.createElement('ul');
-    obj.forEach(list => {
-        let name = document.createElement('li');
-        name.innerText = list.listName;
-        name.addEventListener('click', e => {expandList(e)})
-        listList.appendChild(name);
-    })
-    return listList;
-}
+    let actors = document.createElement('h3');
+    actors.innerText = `Starring: ${obj.Actors}`;
+    card.appendChild(actors);
 
-function expandList(event){
-    fetch('http://localhost:3000/lists')
-    .then(resp => resp.json())
-    .then(json => {
-        json.forEach(list => {
-            if (list.listName === event.target.innerText){
-                let movieList = document.createElement('ul');
-                list.movies.forEach(movie => {
-                    let title = document.createElement('li');
-                    title.innerText = movie.Title;
-                    movieList.appendChild(title);
-                })
-                event.target.appendChild(movieList)
-            }
+    let plot = document.createElement('p');
+    plot.innerText = obj.Plot;
+    card.appendChild(plot);
+
+    let released = document.createElement('span');
+    released.innerText = `Released: ${obj.Released}`;
+    card.appendChild(released);
+
+    let genre = document.createElement('span');
+    genre.innerText = `Genre: ${obj.Genre}`;
+    card.appendChild(genre);
+
+
+    let imdb = document.createElement('p')
+    imdb.innerText = `IMDB Rating: ${obj.Ratings[0].Value}`
+    card.appendChild(imdb);
+
+    let metascore = document.createElement('p');
+    metascore.innerText = `Metascore: ${obj.Ratings[2].Value}`;
+    card.appendChild(metascore);
+
+    let rottenTomatoes = document.createElement('p')
+    rottenTomatoes.innerText = `Rotten Tomatoes: ${obj.Ratings[1].Value}`;
+    card.appendChild(rottenTomatoes);
+
+    return card;
+    }
+
+    function makeList(obj){
+        let listList = document.createElement('ul');
+        obj.forEach(list => {
+            let name = document.createElement('li');
+            name.innerText = list.listName;
+            name.addEventListener('click', e => {expandList(e)})
+            listList.appendChild(name);
         })
-    })
-}
+        return listList;
+    }
+
+    function expandList(event){
+        fetch('http://localhost:3000/lists')
+        .then(resp => resp.json())
+        .then(json => {
+            json.forEach(list => {
+                if (list.listName === event.target.innerText){
+                    let movieList = document.createElement('ul');
+                    list.movies.forEach(movie => {
+                        let title = document.createElement('li');
+                        title.innerText = movie.Title;
+                        title.addEventListener('click', e => {
+                            fetch('http://localhost:3000/lists')
+                            .then(resp => resp.json())
+                            .then(json => {
+                                json.forEach(list => {
+                                    for (let key in list){
+                                        if (key === 'movies'){
+                                            let movies = list[key];
+                                            for (let film in movies){
+                                                let films = movies[film]
+                                                for (info in films){
+                                                    if (info === 'Title'){
+                                                        if (films[info] === e.target.innerText){
+                                                            cardContainer.innerHTML = '';
+                                                            cardContainer.appendChild(createMovieCard(films))
+                                                        }
+                                                    }
+                                                }
+                                            }
+                                        }
+                                    }
+                                })
+                            })
+                        })
+                        movieList.appendChild(title);
+                    })
+                    event.target.appendChild(movieList)
+                }
+            })
+        })
+    }
+})
